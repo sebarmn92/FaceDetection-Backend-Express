e482bba51d42698f64996c22fd61d56511fd4263
sebarmn92 <129097998+sebarmn92@users.noreply.github.com>
2025-09-08 19:28:21 +0300

    Initial version

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PAT = 'c202b98b870747779bb281dee2f392e6';
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id:'123',
+            name:'john',
+            email:'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+
+    res.send('this is working');
+});
+
+app.post('/detectface', (req, res) => {
+    const {imgurl} = req.body;
+    const requestOptions = {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Authorization': 'Key ' + PAT
+                        },
+                        body: JSON.stringify({
+                                            "user_app_id": {
+                                                "user_id": USER_ID,
+                                                "app_id": APP_ID
+                                            },
+                                            "inputs": [
+                                                {
+                                                    "data": {
+                                                        "image": {
+                                                            "url": imgurl
+                                                            // "base64": IMAGE_BYTES_STRING
+                                                        }
+                                                    }
+                                                }
+                                            ]
+                                        })
+                    };
+
+     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response?.json())
+    .then(result => {
+      
+        let responseArr = [];
+        const regions = result.outputs[0].data.regions;
+
+        regions.forEach ( region => {
+        const boundingBox = region.region_info.bounding_box;
+
+        responseArr.push(
+            {
+                'topRow' : boundingBox.top_row.toFixed(3),
+                'leftCol' : boundingBox.left_col.toFixed(3),
+                'bottomRow' : boundingBox.bottom_row.toFixed(3),
+                'rightCol' : boundingBox.right_col.toFixed(3)
+            }
+        );
+        })
+                    
+        res.send(
+            JSON.stringify(responseArr)
+        );
+        }).catch(e => {
+        console.log(e);
+        res.send(e);
+        })
+});
+
+
+app.listen(3000, () => {
+    console.log("running on port 3000");
+});
