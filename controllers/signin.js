f30a2e49643a9998f8390f90056ac5c62f7a5ce4
sebarmn92 <129097998+sebarmn92@users.noreply.github.com>
2025-10-26 22:56:14 +0200

    authorization token handling

--- controllers/signin.js
+++ controllers/signin.js
@@ -26,8 +26,16 @@ const handleSignin = (req, response, db, bcrypt) => {
         }).catch((e) => Promise.reject(e, 'Internal error. Contact administrator'))
 }
 
-const getAuthTokenId = () => {
-    console.log('auth ok')
+const getAuthTokenId = (req, res, db) => {
+    const {authorization} = req.headers;
+
+    return db('auth').where('token', authorization).then((data) => {
+        if((data.length === 1) && (data[0].token == authorization)){
+            return db('users').where('email', data[0].email.toLowerCase()).then( (data) => { return data[0] } ).catch(err => Promise.reject(err))
+        }else{
+            return Promise.reject(res.status(400).json('unauthorized'))
+        }
+    }).catch(err => Promise.reject(err))
 }
 
 const signToken = (email) => {
@@ -35,29 +43,33 @@ const signToken = (email) => {
     return jwt.sign(jwtPayload, process.env.JWT_SECRET, {expiresIn: '2 days'})
 }
 
-const setToken = () => {
-    //store jwt. researching alternativ to redis due to installer not working on windows 11 
-}
-
-const createSessions = (user) => {
+const createSessions = (user,db) => {
     //JWT & return user data
     const {id, email} = user;
     const token = signToken(email);
-    return {
-        'success' : true,
-        'userId' : id, 
-        token
-    }
+
+    return db('auth').where('email', email.toLowerCase()).del(['email', 'token'], {includeTriggerModification : true}).then(() =>{
+        return db('auth').insert({
+                    'email' : email,
+                    'token' : token
+                    }).then(() =>{
+                            return {
+                                    'success' : true,
+                                    'profile' : user, 
+                                    token
+                                }
+                    }).catch(err => Promise.reject(err))
+    }).catch(err => Promise.reject(err))
 }
 
 const signinAuthentification = (db, bcrypt) => (req, response) => {
-    const {auth} = req.headers;
-    
-    return auth ? 
-    getAuthTokenId() : 
+    const {authorization} = req.headers;
+
+    return authorization ? 
+    getAuthTokenId(req, response, db).then((data) =>  { response.json(data) } ).catch(err => Promise.reject(err) ) : 
     handleSignin(req, response, db, bcrypt)
     .then((data) => {
-        return data.id && data.email ? createSessions(data) : Promise.reject(data)
+        return data.id && data.email ? createSessions(data,db) : Promise.reject(data)
     })
     .then(session => response.json(session))
     .catch(err => response.status(400).json(err))
